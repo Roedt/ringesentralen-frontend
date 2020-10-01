b2d0998ef3e76f22cd0f8f2c33d5ecf46f523bce
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-01 20:04:47 +0200

    Legger til dummy ring side

---
+++ pages/ring.js
@@ -0,0 +1,14 @@
+const Ring = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-4xl font-mono'>Ringesentralen</h1>
+        <div>
+          Ring noen
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Ring
