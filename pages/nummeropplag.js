c8d7d898b064c8b0f53a85abfa76455b408ff259
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-01 20:06:30 +0200

    Legger inn dummy for nummeroppslag

---
+++ pages/nummeropplag.js
@@ -0,0 +1,14 @@
+const Nummeroppslag = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-4xl font-mono'>Ringesentralen</h1>
+        <div>
+          Slå opp nummer
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Nummeroppslag
