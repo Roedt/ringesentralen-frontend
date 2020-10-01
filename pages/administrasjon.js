38faf67132c8b1f1a8ae13bffbefe2a81eafd12a
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-01 20:11:59 +0200

    Placeholder for administrasjon

---
+++ pages/administrasjon.js
@@ -0,0 +1,14 @@
+const Administrasjon = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-4xl font-mono'>Ringesentralen</h1>
+        <div>
+          Administrasjon
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Administrasjon
