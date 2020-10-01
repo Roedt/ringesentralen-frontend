adbfc63673fbf20ae56019c2837d5eff6e36463e
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-01 20:10:14 +0200

    Side for statistikk

---
+++ pages/statistikk.js
@@ -0,0 +1,14 @@
+const Statistikk = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-4xl font-mono'>Ringesentralen</h1>
+        <div>
+          Statistikk
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Statistikk
