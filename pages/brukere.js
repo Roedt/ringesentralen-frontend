0ffbba646093041ba7fbe9df8f4754cc02d17d41
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-01 20:09:03 +0200

    Legger til dummyside for brukere

---
+++ pages/brukere.js
@@ -0,0 +1,14 @@
+const Brukere = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-4xl font-mono'>Ringesentralen</h1>
+        <div>
+          Bruker
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Brukere
