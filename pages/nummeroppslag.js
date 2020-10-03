3278484e6478270d4e2c596960ad327a9837ac6f
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-03 15:42:20 +0200

    Legger inn dummy søkefelt

--- pages/nummeroppslag.js
+++ pages/nummeroppslag.js
@@ -7,8 +7,14 @@ const Nummeroppslag = () => {
       <Head>
         <title>Nummeroppslag</title>
       </Head>
-      <div>
-        Slå opp nummer
+      <div className='shadow flex'>
+        <input className='w-full rounded p-2' type='text' placeholder='Tast inn telefonnummer' />
+        <button className='bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400'>
+          <svg width='1em' height='1em' viewBox='0 0 16 16' className='bi bi-search' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+            <path fill-rule='evenodd' d='M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z' />
+            <path fill-rule='evenodd' d='M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z' />
+          </svg>
+        </button>
       </div>
     </Layout>
   )
