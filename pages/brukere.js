c2bab89f8b78f9aa2d9d358f1ddb3c33d31c3098
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-06 20:27:48 +0100

    Bedre feilhåndtering på parse-cookie og henting av brukere

--- pages/brukere.js
+++ pages/brukere.js
@@ -1,7 +1,38 @@
+import axios from 'axios'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Layout from '../components/layout'
 
+const is401 = error => {
+  return /401/.test(error.message)
+}
+
+const Bruker = ({ epost }) => {
+  return (
+    <div>
+      {epost}
+    </div>
+  )
+}
+
 const Brukere = () => {
+  const router = useRouter()
+  const [brukere, setBrukere] = useState()
+
+  async function handleBrukere () {
+    try {
+      const { data } = await axios.get('/api/brukere', { withCredentials: true })
+      setBrukere(data)
+    } catch (error) {
+      if (is401(error)) {
+        router.push('/login')
+      } else {
+        console.error(error)
+      }
+    }
+  }
+
   return (
     <Layout>
       <Head>
@@ -14,11 +45,12 @@ const Brukere = () => {
         Godkjenn nye brukere
       </div>
       <div>
-        Registrerte brukere
+        <button onClick={() => handleBrukere()}>Registrerte brukere</button>
       </div>
       <div>
         Deaktiverte/Ikke godkjente brukere
       </div>
+      {brukere && brukere.map(bruker => <Bruker {...bruker} key={bruker.hypersysID} />)}
     </Layout>
   )
 }
