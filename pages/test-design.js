658bf14f5a3e711e82f12f681f632bf4c573b07d
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-07 15:28:52 +0100

    Legger til åpne/lukke på hamburgermenyen

--- pages/test-design.js
+++ pages/test-design.js
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 function Test (request, response) {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
       <nav className='bg-gray-800'>
@@ -21,8 +24,11 @@ function Test (request, response) {
               </div>
             </div>
             <div className='-mr-2 flex md:hidden'>
-              <button className='bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
-                <span className='sr-only'>Open main menu</span>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className='bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+              >
+                <span className='sr-only'>Åpne hovedmenyen</span>
                 <svg className='block h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
                   <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />
                 </svg>
@@ -33,13 +39,15 @@ function Test (request, response) {
             </div>
           </div>
         </div>
-        <div className='hidden md:hidden'>
+        <div className={`${isOpen ? '' : 'hidden'} md:hidden`}>
           <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-            <a href='#' className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'>Dashboard</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Team</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Projects</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Calendar</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Reports</a>
+            <a href='#' className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'>Ringesentralen</a>
+            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Ring neste</a>
+            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Nummeroppslag</a>
+            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Hjelp</a>
+            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Brukere</a>
+            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Statistikk</a>
+            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Administrasjon</a>
           </div>
         </div>
       </nav>
