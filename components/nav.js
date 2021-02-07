e4430e2b779bc688aec7d5fa482449f78394b866
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-07 19:57:42 +0100

    Fikser atributter pga... react

--- components/nav.js
+++ components/nav.js
@@ -44,10 +44,10 @@ function Nav () {
             >
               <span className='sr-only'>Åpne hovedmenyen</span>
               <svg className='block h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                <path stroke-linecap='round' stroke-linejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16' />
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16' />
               </svg>
               <svg className='hidden h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                <path stroke-linecap='round' stroke-linejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
               </svg>
             </button>
           </div>
