0a3fc322745582f1435879f4838e5ce04e737d8a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-07 19:25:35 +0100

    Legger navigasjon som egen komponent

--- pages/test-design.js
+++ pages/test-design.js
@@ -1,57 +1,9 @@
-import { useState } from 'react'
+import Nav from '../components/nav'
 
 function Test (request, response) {
-  const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
-      <nav className='bg-gray-800'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-          <div className='flex items-center justify-between h-16'>
-            <div className='flex items-center'>
-              <div className='flex-shrink-0'>
-                <img className='h-8 w-8' src='/logo.jpg' alt='Rødt logo' />
-              </div>
-              <div className='hidden md:block'>
-                <div className='ml-10 flex items-baseline space-x-4'>
-                  <a href='#' className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'>Ringesentralen</a>
-                  <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Ring neste</a>
-                  <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Nummeroppslag</a>
-                  <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Hjelp</a>
-                  <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Brukere</a>
-                  <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Statistikk</a>
-                  <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Administrasjon</a>
-                </div>
-              </div>
-            </div>
-            <div className='-mr-2 flex md:hidden'>
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className='bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
-              >
-                <span className='sr-only'>Åpne hovedmenyen</span>
-                <svg className='block h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                  <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />
-                </svg>
-                <svg className='hidden h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                  <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className={`${isOpen ? '' : 'hidden'} md:hidden`}>
-          <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-            <a href='#' className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'>Ringesentralen</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Ring neste</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Nummeroppslag</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Hjelp</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Brukere</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Statistikk</a>
-            <a href='#' className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>Administrasjon</a>
-          </div>
-        </div>
-      </nav>
-
+      <Nav />
       <header className='bg-white shadow-sm'>
         <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8'>
           <h1 className='text-lg leading-6 font-semibold text-gray-900'>
