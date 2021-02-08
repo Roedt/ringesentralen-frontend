9c86b45fec7b584bdbad415e064d53e73aa52cd6
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-08 19:31:15 +0100

    Fjerner gammel test

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import App from '../pages/index'
-
-test('Loads app', () => {
-  render(<App />)
-  expect(screen.getAllByText('Ringesentralen')).toBeTruthy()
-})
