0547d90eb902f9f358cad2f55ca1c99d3b47f216
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-11 17:59:26 +0100

    Setter opp proxy mot backend

---
+++ pages/api/backend/[...remote].js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import parseCookie from '../../../lib/parse-cookie'
+
+async function backendProxy (request, response) {
+  const { query: { remote }} = request
+  const method = request.method.toLowerCase()
+  const payload = await request.body
+  const cookie = parseCookie(request, response)
+  if (!cookie) {
+    response.status(401).json({ isAuthenticated: false })
+  } else {
+    const { token } = cookie
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`
+    const url = `${process.env.API_URL}/${remote.join('/')}`
+    const { data } = await axios[method](url, payload)
+    response.json(data)
+  }
+}
+
+export default backendProxy
