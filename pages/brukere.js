a55015fcf56517d8b61e342e0b2fb8630f236978
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-13 07:28:49 +0100

    Legger inn penere brukerliste

--- pages/brukere.js
+++ pages/brukere.js
@@ -8,10 +8,74 @@ const is401 = error => {
   return /401/.test(error.message)
 }
 
-const Bruker = ({ epost }) => {
+const Bruker = ({ fornavn, etternavn, epost }) => {
   return (
-    <div>
-      {epost}
+    <tr>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='flex items-center'>
+          <div className='ml-4'>
+            <div className='text-sm font-medium text-gray-900'>
+              {fornavn} {etternavn}
+            </div>
+            <div className='text-sm text-gray-500'>
+              {epost}
+            </div>
+          </div>
+        </div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm text-gray-900'>Regional Paradigm Technician</div>
+        <div className='text-sm text-gray-500'>Optimization</div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
+          Active
+        </span>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
+        Admin
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
+        <a href='#' className='text-indigo-600 hover:text-indigo-900'>Edit</a>
+      </td>
+    </tr>
+  )
+}
+
+function BrukerListe ({ brukere }) {
+  if (!brukere) return null
+  return (
+    <div className='flex flex-col'>
+      <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+        <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+          <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+            <table className='min-w-full divide-y divide-gray-200'>
+              <thead className='bg-gray-50'>
+                <tr>
+                  <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                    Name
+                  </th>
+                  <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                    Title
+                  </th>
+                  <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                    Status
+                  </th>
+                  <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                    Role
+                  </th>
+                  <th scope='col' className='relative px-6 py-3'>
+                    <span className='sr-only'>Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className='bg-white divide-y divide-gray-200'>
+                {brukere && brukere.map(bruker => <Bruker {...bruker} key={bruker.hypersysID} />)}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
@@ -50,7 +114,7 @@ const Brukere = () => {
       <div>
         Deaktiverte/Ikke godkjente brukere
       </div>
-      {brukere && brukere.map(bruker => <Bruker {...bruker} key={bruker.hypersysID} />)}
+      <BrukerListe brukere={brukere} />
     </Layout>
   )
 }
