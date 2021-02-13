f8ede7d03db8e6f011d4c8c59285202de2dde057
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-13 07:50:43 +0100

    Fikser for tidlig avrunding av rad

--- pages/brukere.js
+++ pages/brukere.js
@@ -110,7 +110,7 @@ const Brukere = () => {
           <button type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Godkjenn nye brukere
           </button>
-          <button onClick={() => handleBrukere()} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => handleBrukere()} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Registrerte brukere
           </button>
           <button type='button' className='-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
