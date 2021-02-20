e94f02a3ae4f5249f9fcda7b8072648e183f1168
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-20 07:37:51 +0100

    Setter proxy til å viderebringe status fra backend

--- pages/api/backend/[...remote].js
+++ pages/api/backend/[...remote].js
@@ -12,8 +12,8 @@ async function backendProxy (request, response) {
     const { token } = cookie
     axios.defaults.headers.common.Authorization = `Bearer ${token}`
     const url = `${process.env.API_URL}/${remote.join('/')}`
-    const { data } = await axios[method](url, payload)
-    response.json(data)
+    const { data, status } = await axios[method](url, payload)
+    response.status(status).json(data)
   }
 }
 
