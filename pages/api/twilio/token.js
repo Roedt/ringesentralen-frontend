51c3ae9738146dffa69154613f276c12fa87fa5c
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-20 17:24:00 +0100

    Setter opp eget endepunkt for twilio-oppsett

---
+++ pages/api/twilio/token.js
@@ -0,0 +1,35 @@
+import parseCookie from '../../../lib/parse-cookie'
+
+function twilioToken (request, response) {
+  const cookie = parseCookie(request, response)
+  if (!cookie) {
+    response.status(401).json({ isAuthenticated: false })
+  } else {
+    const AccessToken = require('twilio').jwt.AccessToken
+    const VoiceGrant = AccessToken.VoiceGrant
+    // Oppsett for Twilio
+    const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID
+    const twilioApiKey = process.env.TWILIO_API_KEY
+    const twilioApiSecret = process.env.TWILIO_API_SECRET
+    // Oppsett for denne appen
+    const outgoingApplicationSid = process.env.TWILIO_TWIML_APP_SID
+    const identity = process.env.TWILIO_CALLER_ID
+    // Grant som setter opp voice som denne brukeren
+    const voiceGrant = new VoiceGrant({
+      outgoingApplicationSid: outgoingApplicationSid,
+      incomingAllow: false
+    })
+    // Lager tokenet som skal bruker av klienten
+    const token = new AccessToken(
+      twilioAccountSid,
+      twilioApiKey,
+      twilioApiSecret,
+      { identity: identity }
+    )
+    token.addGrant(voiceGrant)
+    // Serialize the token to a JWT string
+    response.send(token.toJwt())
+  }
+}
+
+export default twilioToken
