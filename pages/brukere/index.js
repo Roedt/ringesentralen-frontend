babdda3737d4b8b57b5beb3be24e808a0d70dd8b
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-23 07:47:08 +0100

    Henter alle bruker ved last av siden

--- pages/brukere/index.js
+++ pages/brukere/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Layout from '../../components/layout'
 import BrukerListe from './bruker-liste'
 
@@ -13,7 +13,7 @@ const Brukere = () => {
   const router = useRouter()
   const [brukere, setBrukere] = useState()
 
-  async function handleBrukere () {
+  async function hentBrukere () {
     try {
       const { data } = await axios.get('/api/backend/brukere/brukere', { withCredentials: true })
       setBrukere(data)
@@ -26,23 +26,27 @@ const Brukere = () => {
     }
   }
 
+  useEffect(() => {
+    hentBrukere()
+  }, [])
+
   return (
     <Layout pageTitle='Brukere'>
       <Head>
         <title>Brukere</title>
       </Head>
-      <div className='flex mb-8'>
+      <div className='flex justify-end mb-8'>
         <span className='relative z-0 inline-flex shadow-sm rounded-md'>
-          <button type='button' className='relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
-            Brukerhåndtering
+          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+            Vis alle brukere
           </button>
-          <button type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Godkjenn nye brukere
           </button>
-          <button onClick={() => handleBrukere()} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Registrerte brukere
           </button>
-          <button type='button' className='-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Deaktiverte/Ikke godkjente brukere
           </button>
         </span>
