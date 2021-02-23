2558ee24a5ea7a610bdc69bfcfffdbbf07628849
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-23 13:30:37 +0100

    Legger til håndtering av videresending til et nummer om noen ringer tilbake

--- pages/api/twilio/connect.js
+++ pages/api/twilio/connect.js
@@ -2,12 +2,20 @@ import twilio from 'twilio'
 import fixTelefonNummer from '../../../lib/fix-telefonnummer'
 const VoiceResponse = twilio.twiml.VoiceResponse
 
+const erUtGaaende = caller => /client/.test(caller)
+
 async function twilioConnectCall (request, response) {
   const payload = await request.body
-  const { telefonnummer } = payload
+  const { telefonnummer, Caller } = payload
   const twiml = new VoiceResponse()
   const dial = twiml.dial({ callerId: process.env.TWILIO_CALLER_ID })
-  dial.number({}, fixTelefonNummer(telefonnummer))
+  if (erUtGaaende(Caller)) {
+    // Telefoner fra Ringesentralen over VoIP
+    dial.number({}, fixTelefonNummer(telefonnummer))
+  } else {
+    // Telefoner sok ringer tilbake og skal redirectes til et av våre nummere
+    dial.number({}, process.env.TWILIO_INCOMING_HANDLER)
+  }
   response.setHeader('Content-Type', 'application/xml')
   response.status(200).send(twiml.toString())
 }
