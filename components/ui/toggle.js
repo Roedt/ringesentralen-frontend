6184242128f7f762e3ae6feb0f1ca6a19575f485
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-24 07:46:43 +0100

    Fixes #10 kjøre funksjoner ved toggle av og på

--- components/ui/toggle.js
+++ components/ui/toggle.js
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-function Toggle ({ skjermleserTekst, status }) {
+const isFunction = func => typeof func === 'function'
+
+function Toggle ({ skjermleserTekst, status, runIfOn, runIfOff }) {
   const [aktiv, setAktiv] = useState(status || false)
   function toggleAktiv () {
     setAktiv(!aktiv)
-    console.log(aktiv)
   }
+
+  useEffect(() => {
+    if (aktiv && isFunction(runIfOn)) {
+      console.log('kjører funksjon for på')
+      runIfOn()
+    }
+    if (!aktiv && isFunction(runIfOff)) {
+      console.log('kjører funksjon for av')
+      runIfOn()
+    }
+  }, [aktiv])
 
   const buttonEnabled = 'bg-indigo-600 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
   const buttonDisabled = 'bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
