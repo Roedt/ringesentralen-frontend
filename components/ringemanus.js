05d3e96cdd3ccba2e578a319beff9834a7a27d3e
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-25 09:18:25 +0100

    Gjør vis/skjul knappen lik de andre knappene

--- components/ringemanus.js
+++ components/ringemanus.js
@@ -26,14 +26,12 @@ function Ringemanus ({ manus }) {
 
   return (
     <div className='prose lg:prose-xl'>
-      <button onClick={toggleManus} className='text-left w-full flex justify-between items-start text-gray-400'>
-        <span className='font-medium text-gray-900'>
+      <button onClick={toggleManus} className='w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+        <svg className={`${visManus ? '-rotate-180' : 'rotate-0'} -ml-1 mr-2 h-5 w-5 text-gray-400 transform`} xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+          <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 9l-7 7-7-7' />
+        </svg>
+        <span>
           {visManus ? 'Skjul' : 'Vis'} ringemanus
-        </span>
-        <span className='ml-6 h-7 flex items-center'>
-          <svg className={`${visManus ? 'rotate-0' : '-rotate-180'} h-6 w-6 transform`} xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-            <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 9l-7 7-7-7' />
-          </svg>
         </span>
       </button>
       <div className={`${visManus ? 'visible' : 'invisible'}`} dangerouslySetInnerHTML={{ __html: html }} />
