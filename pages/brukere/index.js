bd19d011fc95e2e2f05c6ca8b11a36d4873f2c4a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-28 09:01:19 +0100

    Fixes #8 legger filter på uthenting av brukere

--- pages/brukere/index.js
+++ pages/brukere/index.js
@@ -12,11 +12,17 @@ const is401 = error => {
 const Brukere = () => {
   const router = useRouter()
   const [brukere, setBrukere] = useState()
+  const [filterKriterie, setFilterKriterie] = useState()
 
   async function hentBrukere () {
     try {
       const { data } = await axios.get('/api/backend/brukere/brukere', { withCredentials: true })
-      setBrukere(data)
+      if (filterKriterie) {
+        const filtrert = data.filter(bruker => bruker.rolle.includes(filterKriterie))
+        setBrukere(filtrert)
+      } else {
+        setBrukere(data)
+      }
     } catch (error) {
       if (is401(error)) {
         router.push('/login')
@@ -28,7 +34,7 @@ const Brukere = () => {
 
   useEffect(() => {
     hentBrukere()
-  }, [])
+  }, [filterKriterie])
 
   return (
     <Layout pageTitle='Brukere'>
@@ -37,16 +43,16 @@ const Brukere = () => {
       </Head>
       <div className='flex justify-end mb-8'>
         <span className='relative z-0 inline-flex shadow-sm rounded-md'>
-          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => setFilterKriterie(false)} type='button' className='relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Vis alle brukere
           </button>
-          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => setFilterKriterie('venter_paa_godkjenning')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Godkjenn nye brukere
           </button>
-          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => setFilterKriterie('bruker')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Registrerte brukere
           </button>
-          <button onClick={() => window.alert('ikke implementert ennå')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
+          <button onClick={() => setFilterKriterie('sperret')} type='button' className='-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'>
             Deaktiverte/Ikke godkjente brukere
           </button>
         </span>
