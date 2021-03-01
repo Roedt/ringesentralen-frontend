961f0d96f77333729f588c75bd0f31aedf410723
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-01 18:00:51 +0100

    Fixes #25 format på dato i samtaleliste

--- pages/minesamtaler/samtale.js
+++ pages/minesamtaler/samtale.js
@@ -1,7 +1,9 @@
+import prettyPrintDate from '../../lib/prettyprint-dato'
+
 const Samtale = ({ tidspunkt, ringer, kommentar, resultat, ringtNummer, ringtNavn }) => {
   return (
     <tr className='text-sm font-medium text-gray-900'>
-      <td className='px-6 py-4 whitespace-nowrap'>{tidspunkt}</td>
+      <td className='px-6 py-4 whitespace-nowrap'>{prettyPrintDate(tidspunkt)}</td>
       <td className='px-6 py-4 whitespace-nowrap'>{ringtNavn} ({ringtNummer})</td>
       <td className='px-6 py-4 whitespace-nowrap'>{resultat}</td>
       <td className='px-6 py-4 whitespace-nowrap'>{kommentar}</td>
