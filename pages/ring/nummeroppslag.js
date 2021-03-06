5c3bd8b37891be15119ab1938d539403dd3c7b96
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-06 08:39:50 +0100

    Legger til ferdige alertbokser

--- pages/ring/nummeroppslag.js
+++ pages/ring/nummeroppslag.js
@@ -25,7 +25,7 @@ function Nummeroppslag ({ setPerson }) {
         <div className='relative flex items-stretch flex-grow focus-within:z-10'>
           <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
             <svg className='h-5 w-5 text-gray-400' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
-              <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z' />
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z' />
             </svg>
           </div>
           <input type='text' name='telefonnummer' value={telefonNummer} onChange={event => setTelefonNummer(event.target.value)} id='telefonnummer' className='focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300' placeholder='Telefonnummer' />
