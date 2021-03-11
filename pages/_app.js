71fbe28433a98e9600496a15319ca4d9a3b25ce7
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-11 17:14:06 +0100

    Legger til Sentry i _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../css/tailwind.css'
 import 'toasted-notes/src/styles.css'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+import '../css/tailwind.css'
+
+Sentry.init({
+  dsn: 'https://43df61ecc23343df9c559c1e677a3494@o549321.ingest.sentry.io/5671959',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0
+})
 
 export default function MyApp ({ Component, pageProps }) {
   return (
