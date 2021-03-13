40e74b4d86e7b88b769c0a9481ced148caafef86
Mads Opheim <mads@fostad.net>
2021-03-13 16:15:10 +0100

    #45: Debuglogging

--- pages/api/ringemanus.js
+++ pages/api/ringemanus.js
@@ -1,15 +1,25 @@
-import { readFile } from 'fs/promises'
+import { readdirSync } from 'fs';
+import { readFile, readdir } from 'fs/promises'
 import path from 'path'
 
 async function hentRingeManus (request, response) {
   const { manus } = await request.query
+  console.log('manus', manus)
+
+  
   const filmappe = path.join(process.cwd(), 'ringemanus')
+  console.log('cwd', process.cwd())
+  console.log('files i process.cwd()', readdirSync(process.cwd()))
+  console.log('filmappe', filmappe)
+  console.log('files i filmappe', readdirSync(filmappe))
   const fallback = 'fallback.md'
   const filnavn = manus ? `${manus}.md` : fallback
+  console.log('filnavn', filnavn)
   let tekst = ''
   try {
     tekst = await readFile(path.join(filmappe, filnavn), 'utf-8')
   } catch (error) {
+    console.error(error)
     tekst = await readFile(path.join(filmappe, fallback), 'utf-8')
   }
   response.setHeader('Content-Type', 'text/markdown; charset=utf-8')
