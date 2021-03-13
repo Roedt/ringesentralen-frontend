87fa279973c4181d8bfa726dd1f8eaf7494d573c
Mads Opheim <mads@fostad.net>
2021-03-13 18:47:09 +0100

    #45: Tar bort omfattande logging igjen no som ting funkar

--- pages/api/ringemanus/index.js
+++ pages/api/ringemanus/index.js
@@ -1,19 +1,12 @@
-import { readdirSync } from 'fs'
 import { readFile } from 'fs/promises'
 import path from 'path'
 
 async function hentRingeManus (request, response) {
   const { manus } = await request.query
-  console.log('manus', manus)
 
   const filmappe = path.join(process.cwd(), 'pages', 'api', 'ringemanus', 'manus')
-  console.log('cwd', process.cwd())
-  console.log('files i process.cwd()', readdirSync(process.cwd()))
-  console.log('filmappe', filmappe)
-  console.log('files i filmappe', readdirSync(filmappe))
   const fallback = 'fallback.md'
   const filnavn = manus ? `${manus}.md` : fallback
-  console.log('filnavn', filnavn)
   let tekst = ''
   try {
     tekst = await readFile(path.join(filmappe, filnavn), 'utf-8')
