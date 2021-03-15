1d0457982dba56ca42b9eb596fe83ded4513df33
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-15 15:25:36 +0100

    Henter profil og legger ved session ved login

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,7 +4,8 @@ import withSession from '../../lib/session'
 import { is401, is403, is503 } from '../../lib/utils'
 
 async function login (request, response) {
-  const url = `${process.env.API_URL}/token/login`
+  const tokenUrl = `${process.env.API_URL}/token/login`
+  const profilUrl = `${process.env.API_URL}/profil`
   const { brukarnamn, passord } = await request.body
   const payload = {
     brukarnamn,
@@ -12,9 +13,16 @@ async function login (request, response) {
     key: process.env.API_AUTH_KEY
   }
   try {
-    const { data: token } = await axios.post(url, payload)
+    const { data: token } = await axios.post(tokenUrl, payload)
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`
+    const { data: profil } = await axios.get(profilUrl)
+    const { email, rolle, fylke, lokallag } = profil
 
     const user = {
+      email,
+      rolle,
+      fylke,
+      lokallag,
       token,
       expires: new Date().getTime() + 36000000,
       isAuthenticated: true
