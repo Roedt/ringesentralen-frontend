08a8fe49c946a00f9322b93996d2b8cbb84e6127
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-16 16:15:11 +0100

    Fixes #57 logo på login

--- pages/login.js
+++ pages/login.js
@@ -46,9 +46,12 @@ function Login () {
       <div className='flex flex-col h-screen bg-gray-100'>
         <div className='grid place-items-center mx-2 my-20 sm:my-auto'>
           <div className='w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg'>
-            <h2 className='text-center font-semibold text-3xl lg:text-4xl text-gray-800'>
-              Innlogging
-            </h2>
+            <div>
+              <div className='flex flex-row'>
+                <img className='-ml-2 h-10 w-10' src='/logo.jpg' alt='Rødt logo' />
+                <h2 className='text-2xl font-semibold p-1 ml-2'>Ringesentralen</h2>
+              </div>
+            </div>
             <form className='mt-10' id='login-form' onSubmit={handleSubmit}>
               <label htmlFor='brukarnamn' className='block text-xs font-semibold text-gray-600 uppercase'>
                 E-postadresse
