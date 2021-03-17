6ccf2fc5bf4d2a648e07b2e8469cff189706b548
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-17 17:17:36 +0100

    Legger til avbryt sesjonen knapp. fixes #56

--- pages/ring/samtale.js
+++ pages/ring/samtale.js
@@ -16,10 +16,14 @@ function Samtale ({ accepted, data, device, setPerson }) {
     setSamtale('avsluttet')
   }
 
+  async function avbrytSamtale () {
+    setPerson(false)
+  }
+
   const StartKnapp = ({ id }) => {
     return (
       <button type='button' onClick={() => startSamtale(id)} className='w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 16 16' aria-hidden='true'>
+        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
           <path d='M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6 5.883a.5.5 0 0 1 .757-.429l3.528 2.117a.5.5 0 0 1 0 .858l-3.528 2.117a.5.5 0 0 1-.757-.43V5.884z' />
         </svg>
         <span>
@@ -32,12 +36,25 @@ function Samtale ({ accepted, data, device, setPerson }) {
   const StoppKnapp = () => {
     return (
       <button type='button' onClick={() => avsluttSamtale()} className='w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 16 16' aria-hidden='true'>
+        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
           <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z' />
           <path d='M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z' />
         </svg>
         <span>
           Samtalen er ferdig
+        </span>
+      </button>
+    )
+  }
+
+  const AvbrytKnapp = () => {
+    return (
+      <button type='button' onClick={() => avbrytSamtale()} className='w-48 ml-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
+        </svg>
+        <span>
+          Avbryt ringesesjonen
         </span>
       </button>
     )
@@ -57,6 +74,7 @@ function Samtale ({ accepted, data, device, setPerson }) {
         <div className='ml-4 mt-4 flex-shrink-0 flex'>
           {!samtale && <StartKnapp id={id} />}
           {samtale === 'paagaaende' && <StoppKnapp />}
+          <AvbrytKnapp />
         </div>
       </div>
       <div className='mt-4 flex lg:flex-row md:flex-col'>
