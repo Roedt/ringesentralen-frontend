117097c4b8dbd8bbc078845bdd05fc950d74ae28
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-17 17:55:55 +0100

    Styler om login-bildet. Setter logo helt til høyre

--- pages/login.js
+++ pages/login.js
@@ -47,9 +47,9 @@ function Login () {
         <div className='grid place-items-center mx-2 my-20 sm:my-auto'>
           <div className='w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg'>
             <div>
-              <div className='flex flex-row'>
+              <div className='flex flex-row justify-between'>
+                <h2 className='text-2xl font-semibold pt-1 pb-1'>Ringesentralen</h2>
                 <img className='-ml-2 h-10 w-10' src='/logo.jpg' alt='Rødt logo' />
-                <h2 className='text-2xl font-semibold p-1 ml-2'>Ringesentralen</h2>
               </div>
             </div>
             <form className='mt-10' id='login-form' onSubmit={handleSubmit}>
