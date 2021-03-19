994bdbb401a1f8a7ce00565aa67913463e0d8a3e
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-19 16:47:19 +0100

    Fullfører Sentry oppsett. Fixes #49

--- utils/sentry.js
+++ utils/sentry.js
@@ -1,9 +1,17 @@
 import * as Sentry from '@sentry/node'
 import { RewriteFrames } from '@sentry/integrations'
 
+const ignoreErrors = [
+  'TypeError: Failed to fetch',
+  'TypeError: NetworkError when attempting to fetch resource',
+  'TypeError: cancelled',
+  'TypeError: avbrutt'
+]
+
 export const init = () => {
   if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
     const integrations = []
+    const environment = typeof window !== 'undefined' ? window.location.hostname : 'ukjent'
     if (
       process.env.NEXT_IS_SERVER === 'true' &&
       process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR
@@ -28,8 +36,11 @@ export const init = () => {
     Sentry.init({
       enabled: process.env.NODE_ENV === 'production',
       integrations,
+      environment,
+      autoSessionTracking: false,
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-      release: process.env.NEXT_PUBLIC_COMMIT_SHA
+      release: process.env.NEXT_PUBLIC_COMMIT_SHA,
+      ignoreErrors
     })
   }
 }
