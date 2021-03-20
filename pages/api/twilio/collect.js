bb34679692ec6683c199fa73403238a046abe5ef
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-20 08:18:21 +0100

    Legger til telefonsvarer og funksjonalitet for å håndtere ulike svar

---
+++ pages/api/twilio/collect.js
@@ -0,0 +1,26 @@
+import twilio from 'twilio'
+
+const VoiceResponse = twilio.twiml.VoiceResponse
+
+async function twilioCollectCall (request, response) {
+  const payload = await request.body
+  console.log(JSON.stringify(payload, null, 2))
+  console.log('connect')
+  const { Digits } = payload
+  let melding = 'Vi mottok ikke noe svar, men det er helt greit. Godt valg! '
+  if (Digits === '1') {
+    melding = 'Vi ringer deg tilbake når vi er tilbake på sentralen. Godt valg! '
+  }
+  if (Digits === '2') {
+    melding = 'Da skal vi la være å ringe deg tilbake. Godt valg! '
+  }
+  const twiml = new VoiceResponse()
+  twiml.say({
+    voice: 'Polly.Liv',
+    language: 'nb-NO'
+  }, melding)
+  response.setHeader('Content-Type', 'application/xml')
+  response.status(200).send(twiml.toString())
+}
+
+export default twilioCollectCall
--- pages/api/twilio/connect.js
+++ pages/api/twilio/connect.js
@@ -6,6 +6,8 @@ const erUtGaaende = caller => /client/.test(caller)
 
 async function twilioConnectCall (request, response) {
   const payload = await request.body
+  console.log(JSON.stringify(payload, null, 2))
+  console.log('collect')
   const { telefonnummer, Caller } = payload
   const twiml = new VoiceResponse()
   if (erUtGaaende(Caller)) {
@@ -13,8 +15,21 @@ async function twilioConnectCall (request, response) {
     const dial = twiml.dial({ callerId: process.env.TWILIO_CALLER_ID })
     dial.number({}, fixTelefonNummer(telefonnummer))
   } else {
-    // Telefoner som ringer tilbake og skal redirectes til et av våre nummere
-    twiml.dial(process.env.TWILIO_INCOMING_HANDLER)
+    // Telefoner som ringer tilbake skal treffe telefonsvareren vår
+    // Først setter vi opp tastefunksjonen som tar imot input fra tastene
+    const gather = twiml.gather({
+      input: 'dtmf',
+      timeout: 5,
+      numDigits: 1,
+      action: '/api/twilio/collect'
+    })
+    // Så setter vi opp stemmen som skal leses opp først
+    gather.say({
+      voice: 'Polly.Liv',
+      language: 'nb-NO'
+    }, 'Velkommen til Rødts ringesentral. Tast en om du vil at vi skal ringe deg tilbake senere. Tast to om du ikke ønsker å bli ringt igjen')
+    // Dette er den gamle koden for å redirecte til et annet telefonnummer
+    //twiml.dial(process.env.TWILIO_INCOMING_HANDLER)
   }
   response.setHeader('Content-Type', 'application/xml')
   response.status(200).send(twiml.toString())
