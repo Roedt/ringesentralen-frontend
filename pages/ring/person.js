fdd063c42fa7868f0a122b1a3ed5de79156b31d5
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-25 19:44:20 +0100

    Større knapper på ringebiten

--- pages/ring/person.js
+++ pages/ring/person.js
@@ -56,7 +56,7 @@ function Person ({ data, setIsAccepted, setPerson }) {
           </div>
         </div>
         <div className={`${godtatt ? 'hidden ' : 'visible'}ml-4 mt-4 flex-shrink-0 flex`}>
-          <button type='button' onClick={() => godtaSamtale(id)} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <button type='button' onClick={() => godtaSamtale(id)} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
             <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5' />
             </svg>
@@ -64,7 +64,7 @@ function Person ({ data, setIsAccepted, setPerson }) {
               Godta samtale
             </span>
           </button>
-          <button type='button' onClick={() => avslaaSamtale()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <button type='button' onClick={() => avslaaSamtale()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
             <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' />
             </svg>
@@ -74,12 +74,12 @@ function Person ({ data, setIsAccepted, setPerson }) {
           </button>
         </div>
         <div className={`${godtatt ? 'visible' : 'hidden'} ml-4 mt-4 flex-shrink-0 flex`}>
-          <button type='button' onClick={() => avbrytSesjon()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <button type='button' onClick={() => avbrytSesjon()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
             <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
             </svg>
             <span>
-              Avbryt ringesesjonen
+              Avbryt
             </span>
           </button>
         </div>
--- pages/ring/voip.js
+++ pages/ring/voip.js
@@ -63,7 +63,7 @@ function VoIP ({ telefonnummer }) {
 
   const RingeMedVoipKnapp = ({ VoIPActive, telefonnummer }) => {
     return (
-      <button type='button' onClick={() => startVoIPSamtale(telefonnummer)} className={`w-48 mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 ${VoIPActive ? 'bg-green-500' : 'bg-white'} hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+      <button type='button' onClick={() => startVoIPSamtale(telefonnummer)} className={`w-48 mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 ${VoIPActive ? 'bg-green-500' : 'bg-white'} hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
         <svg className='-ml-1 mr-2 h-5 w-5 text-gray-900' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
           <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M16 3h5m0 0v5m0-5l-6 6M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z' />
         </svg>
@@ -76,7 +76,7 @@ function VoIP ({ telefonnummer }) {
 
   const AvsluttVoipKnapp = ({ VoIPActive }) => {
     return (
-      <button type='button' onClick={() => avsluttVoIPSamtale()} className={`w-48 mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 ${VoIPActive ? 'bg-red-200' : 'bg-white'} hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+      <button type='button' onClick={() => avsluttVoIPSamtale()} className={`w-48 mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 ${VoIPActive ? 'bg-red-200' : 'bg-white'} hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
         <svg className='-ml-1 mr-2 h-5 w-5 text-gray-900' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
           <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z' />
         </svg>
@@ -106,7 +106,7 @@ function VoIP ({ telefonnummer }) {
 
   return (
     <div className='mb-4 pb-4 border-b border-gray-200'>
-      <button onClick={() => handleVoipSetup()} className={`${voipReady && 'hidden '}w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+      <button onClick={() => handleVoipSetup()} className={`${voipReady ? 'hidden' : 'visible'} w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
         <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
           <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z' />
         </svg>
