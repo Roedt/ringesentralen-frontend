b2ce0d5527055bc0996491e447278f7cebc9cdc5
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-25 19:27:22 +0100

    Fixes #85 endrer flyt og setter godta samtale til startSamtale

--- pages/ring/person.js
+++ pages/ring/person.js
@@ -1,22 +1,44 @@
+import axios from 'axios'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
+
+import { is401 } from '../../lib/utils'
 import Historikk from './historikk'
 
 function Person ({ data, setIsAccepted, setPerson }) {
+  const router = useRouter()
   const [godtatt, setIsGodtatt] = useState()
 
   function avslaaSamtale () {
     setPerson(false)
   }
 
-  function godtaSamtale () {
+  function avbrytSesjon () {
+    setPerson(false)
+  }
+
+  async function startSamtale (id) {
+    try {
+      await axios.post('/api/backend/samtale/startSamtale', { skalRingesID: id }, { withCredentials: true })
+    } catch (error) {
+      if (is401(error)) {
+        router.push('/login')
+      } else {
+        console.error(error)
+      }
+    }
+  }
+
+  function godtaSamtale (id) {
     setIsAccepted(true)
     setIsGodtatt(true)
+    startSamtale(id)
   }
 
   if (!data) return null
 
   const { person, tidlegareSamtalar } = data
-  const { fornavn, etternavn, telefonnummer } = person
+  const { fornavn, etternavn, telefonnummer, id } = person
 
   return (
     <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
@@ -33,8 +55,8 @@ function Person ({ data, setIsAccepted, setPerson }) {
             </div>
           </div>
         </div>
-        <div className={`${godtatt ? 'hidden ' : ''}ml-4 mt-4 flex-shrink-0 flex`}>
-          <button type='button' onClick={() => godtaSamtale()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+        <div className={`${godtatt ? 'hidden ' : 'visible'}ml-4 mt-4 flex-shrink-0 flex`}>
+          <button type='button' onClick={() => godtaSamtale(id)} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
             <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5' />
             </svg>
@@ -48,6 +70,16 @@ function Person ({ data, setIsAccepted, setPerson }) {
             </svg>
             <span>
               Avslå samtale
+            </span>
+          </button>
+        </div>
+        <div className={`${godtatt ? 'visible' : 'hidden'} ml-4 mt-4 flex-shrink-0 flex`}>
+          <button type='button' onClick={() => avbrytSesjon()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+            <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
+            </svg>
+            <span>
+              Avbryt ringesesjonen
             </span>
           </button>
         </div>
--- pages/ring/samtale.js
+++ pages/ring/samtale.js
@@ -1,66 +1,10 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Ringemanus from '../../components/ringemanus'
 import ResultatSkjema from './resultatskjema'
 import VoIP from './voip'
 
 function Samtale ({ accepted, data, user, setPerson }) {
-  const [samtale, setSamtale] = useState()
   const [modus, setModus] = useState('velgere')
-
-  async function startSamtale (id) {
-    await axios.post('/api/backend/samtale/startSamtale', { skalRingesID: id }, { withCredentials: true })
-    setSamtale('paagaaende')
-  }
-
-  async function avsluttSamtale () {
-    setSamtale('avsluttet')
-  }
-
-  async function avbrytSamtale () {
-    setPerson(false)
-  }
-
-  const StartKnapp = ({ id }) => {
-    return (
-      <button type='button' onClick={() => startSamtale(id)} className='w-48 mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z' />
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
-        </svg>
-        <span>
-          Samtalen starter
-        </span>
-      </button>
-    )
-  }
-
-  const StoppKnapp = () => {
-    return (
-      <button type='button' onClick={() => avsluttSamtale()} className='w-48 mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z' />
-        </svg>
-        <span>
-          Samtalen er ferdig
-        </span>
-      </button>
-    )
-  }
-
-  const AvbrytKnapp = () => {
-    return (
-      <button type='button' onClick={() => avbrytSamtale()} className='w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-        <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
-        </svg>
-        <span>
-          Avbryt ringesesjonen
-        </span>
-      </button>
-    )
-  }
 
   useEffect(() => {
     if (user) {
@@ -78,18 +22,11 @@ function Samtale ({ accepted, data, user, setPerson }) {
   return (
     <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
       {telefonnummer && <VoIP telefonnummer={telefonnummer} />}
-      <div className='-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap'>
-        <div className='ml-4 mt-4 flex-shrink-0 flex'>
-          {!samtale && <StartKnapp id={id} />}
-          {samtale === 'paagaaende' && <StoppKnapp />}
-          <AvbrytKnapp />
-        </div>
-      </div>
       <div className='mt-4 flex lg:flex-row md:flex-col'>
         <div className='flex-1 lg:pr-4 md:mb-4'>
           <ResultatSkjema id={id} setPerson={setPerson} isOpen modus={modus} />
         </div>
-        <div className='flex-1 lg:pl-4'>
+        <div className='flex-1 lg:pl-4 md:mb-4'>
           <Ringemanus manus={fylke} modus={modus} isOpen />
         </div>
       </div>
