95fb6283d80251771c9c500fef7fecd0dda10dd5
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-27 08:17:00 +0100

    Fixes #94 viser roller på sperretsiden

--- pages/ring/voip.js
+++ pages/ring/voip.js
@@ -111,7 +111,7 @@ function VoIP ({ telefonnummer }) {
     }
 
     return (
-      <div className={`mt-2 flex items-center text-gray-900 font-medium text-l ${statusFarger[status]} py-2`}>
+      <div className={`mt-2 flex items-center text-gray-900 font-medium text-lg ${statusFarger[status]} py-2`}>
         <svg xmlns='http://www.w3.org/2000/svg' className='ml-3 mr-2 h-5 w-5 text-gray-900' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
           <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
         </svg>
--- pages/sperret.js
+++ pages/sperret.js
@@ -1,16 +1,38 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
+import useUser from '../lib/useUser'
 import Layout from '../components/layout'
 
 function Sperret () {
+  const { user } = useUser({ redirectTo: '/login' })
+  const [roller, setRoller] = useState()
+
+  useEffect(() => {
+    if (user) {
+      setRoller(user.rolle)
+    }
+  }, [user])
+
   return (
     <Layout pageTitle='Ingen tilgang'>
       <Head>
         <title>Ingen tilgang</title>
       </Head>
-      <div>
-        Du har ikke tilgang til det du forsøkte å gjøre.
-
-        Dette kan bety at du ikke er godkjent som ringer i systemet ennå eller at du ikke har fått tildelt nødvendig tilgangsnivå.
+      <div className='text-lg'>
+        {JSON.stringify}
+        <p className='mb-2'>
+          Du har ikke tilgang til det du forsøkte å gjøre.
+        </p>
+        <p className='mb-2'>
+          Dette kan bety at du ikke er godkjent som ringer i systemet ennå eller at du ikke har fått tildelt nødvendig tilgangsnivå.
+        </p>
+        <p className='mb-2'>
+          Du har nå disse rollene i løsningen: <span className='text-gray-900 font-semibold'>{roller ? roller.join(', ') : 'ingen'}</span>
+        </p>
+        <p>
+          Mener du dette er feil, eller har spørsmål så <a className='underline tracking-wide text-gray-700 hover:bg-gray-100 hover:text-gray-900' href='https://roedtorg.slack.com/archives/C01BNKD2RU0'>kontakt oss på Slack</a>.
+        </p>
       </div>
     </Layout>
   )
