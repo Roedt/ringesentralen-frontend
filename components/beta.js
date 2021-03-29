778bc1ad3c4721c7d058b654808c1c6dfa6c0a29
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-03-29 16:06:50 +0200

    Legger til beta-melding #104 (patch)

---
+++ components/beta.js
@@ -0,0 +1,18 @@
+function Beta () {
+  return (
+    <div className='flex justify-center my-4 p-6 sm:px-10 sm:py-6 bg-white border-red-900 rounded-lg shadow-md lg:shadow-lg'>
+      <h1 className='text-lg font-semibold text-red-700'>Beta</h1>
+      <div className='mx-4'>
+        <p>
+          Dette er den nyeste versjonen av Ringesentralen og vi utvikler den fortløpende.
+        </p>
+        <p>
+          Dersom du opplever feil eller rare ting så <a className='underline tracking-wide text-gray-700 hover:bg-gray-100 hover:text-gray-900' href='https://roedtorg.slack.com/archives/C01BNKD2RU0'>kontakt oss på Slack</a>
+        </p>
+      </div>
+      <h1 className='text-lg font-semibold text-red-700'>Beta</h1>
+    </div>
+  )
+}
+
+export default Beta
--- pages/login.js
+++ pages/login.js
@@ -7,6 +7,7 @@ import { is401, is403, is503 } from '../lib/utils'
 import generatePayload from '../lib/generate-payload'
 import Button from '../components/ui/button'
 import { Warning } from '../components/ui/alerts'
+import Beta from '../components/beta'
 
 function Login () {
   const [loading, setLoading] = useState()
@@ -42,6 +43,7 @@ function Login () {
         <title>Ringesentralen</title>
       </Head>
       <div className='flex flex-col h-screen bg-gray-100'>
+        <Beta />
         <div className='grid place-items-center mx-2 my-20 sm:my-auto'>
           <div className='w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg'>
             <div>
