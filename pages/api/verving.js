45bbcb6f30ebedc9809d4799d3dcc9148bc57d17
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-03 08:24:45 +0200

    Legger til utsending av sms på endepunktet for verving #97 (patch)

--- pages/api/verving.js
+++ pages/api/verving.js
@@ -1,5 +1,55 @@
+import Twilio from 'twilio'
+
+import fixTelefonNummer from '../../lib/fix-telefonnummer'
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const token = process.env.TWILIO_AUTH_TOKEN
+const callerId = process.env.TWILIO_SMS_ID
+
+function handleSMS (telefonnummer, melding) {
+  const client = new Twilio(accountSid, token)
+  return new Promise((resolve, reject) => {
+    client.messages.create({
+      body: melding,
+      from: callerId,
+      to: fixTelefonNummer(telefonnummer)
+    }).then(result => {
+      resolve(result)
+    }).catch(error => {
+      console.error(error)
+      reject(error)
+    })
+  })
+}
+
+function isSpam (payload) {
+  return payload.epost.length > 0
+}
+
+function genererMelding (payload) {
+  const { ververMegSelv, ververensNavn } = payload
+  const melding = `${ververMegSelv ? 'Du, eller noen andre,' : ververensNavn} har vervet deg til Rødts ringesentral. Dersom du ønsker at noen av Rødts medlemmer skal ringe deg så svarer du "ja" på denne meldingen. Vil du ikke bli ringt svarer du "nei".`
+  return melding
+}
+
+async function sendSMS (payload) {
+  const { telefonnummer } = payload
+  const melding = genererMelding(payload)
+  try {
+    const result = await handleSMS(telefonnummer, melding)
+    return { ...result, success: true }
+  } catch (error) {
+    return { success: false }
+  }
+}
+
 async function verving (request, response) {
-  response.json({ success: true })
+  const payload = await request.body
+  if (!isSpam(payload)) {
+    await sendSMS(payload)
+  }
+  console.log(JSON.stringify(payload, null, 2))
+  response.json({ ...payload, success: true })
 }
 
 export default verving
