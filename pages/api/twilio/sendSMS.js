e102eea832c4c87f35080710854377c2a09eb6ba
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-04 09:05:32 +0200

    Fixes #133 trekke ut sendSMS som egen metode (patch)

--- pages/api/twilio/sendSMS.js
+++ pages/api/twilio/sendSMS.js
@@ -1,27 +1,6 @@
-import Twilio from 'twilio'
-
 import withSession from '../../../lib/session'
 import fixTelefonNummer from '../../../lib/fix-telefonnummer'
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID
-const token = process.env.TWILIO_AUTH_TOKEN
-const callerId = process.env.TWILIO_SMS_ID
-
-function handleSMS (telefonnummer, melding) {
-  const client = new Twilio(accountSid, token)
-  return new Promise((resolve, reject) => {
-    client.messages.create({
-      body: melding,
-      from: callerId,
-      to: fixTelefonNummer(telefonnummer)
-    }).then(result => {
-      resolve(result)
-    }).catch(error => {
-      console.error(error)
-      reject(error)
-    })
-  })
-}
+import handleSMS from '../../../lib/sendSMS'
 
 async function sendSMS (request, response) {
   const user = request.session.get('user')
@@ -31,7 +10,7 @@ async function sendSMS (request, response) {
     const payload = await request.body
     const { telefonnummer, melding } = payload
     try {
-      const result = await handleSMS(telefonnummer, melding)
+      const result = await handleSMS(fixTelefonNummer(telefonnummer), melding)
       response.json(result)
     } catch (error) {
       response.json({ success: false, error })
--- pages/api/verv/sendSMS.js
+++ pages/api/verv/sendSMS.js
@@ -1,24 +1,4 @@
-import Twilio from 'twilio'
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID
-const token = process.env.TWILIO_AUTH_TOKEN
-const callerId = process.env.TWILIO_SMS_ID
-
-function handleSMS (telefonnummer, melding) {
-  const client = new Twilio(accountSid, token)
-  return new Promise((resolve, reject) => {
-    client.messages.create({
-      body: melding,
-      from: callerId,
-      to: telefonnummer
-    }).then(result => {
-      resolve(result)
-    }).catch(error => {
-      console.error(error)
-      reject(error)
-    })
-  })
-}
+import handleSMS from '../../../lib/sendSMS'
 
 function genererMelding (payload) {
   const { ververMegSelv, ververensNavn } = payload
