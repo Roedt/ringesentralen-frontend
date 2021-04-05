8f106cece1fb214832a894a809853e6fffccc55b
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-05 08:05:43 +0200

    Fikser landmarks #134 (patch)

--- pages/loggut.js
+++ pages/loggut.js
@@ -30,7 +30,7 @@ function Loggut () {
         <title>Ringesentralen - logg ut</title>
       </Head>
       <div className='flex flex-col h-screen bg-gray-100'>
-        <div className='grid place-items-center mx-2 my-20 sm:my-auto'>
+        <main className='grid place-items-center mx-2 my-20 sm:my-auto'>
           <div className='w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg'>
             <h1 className='text-center font-semibold text-2xl lg:text-3xl text-gray-800'>
               Logg ut av Ringesentralen
@@ -61,7 +61,7 @@ function Loggut () {
             tekst='Hvordan var opplevelsen din av Ringesentralen i dag?'
             tema='utlogging'
           />
-        </div>
+        </main>
       </div>
     </div>
   )
