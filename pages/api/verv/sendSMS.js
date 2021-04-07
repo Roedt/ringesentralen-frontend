37f85a0f3b1e7b2e0b213e0b19cfc21b488d2476
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-07 16:52:36 +0200

    Bruker toveis nummer for sms ved verving #97 (patch)

--- pages/api/verv/sendSMS.js
+++ pages/api/verv/sendSMS.js
@@ -1,5 +1,7 @@
 import handleSMS from '../../../lib/sendSMS'
 
+const callerId = process.env.TWILIO_SMS_NUMMER
+
 function genererMelding (payload) {
   const { ververMegSelv, ververensNavn } = payload
   const melding = `${ververMegSelv ? 'Du, eller noen andre,' : ververensNavn} har vervet deg til Rødts ringesentral. Dersom du ønsker at noen av Rødts medlemmer skal ringe deg så svarer du "ja" på denne meldingen. Vil du ikke bli ringt svarer du "nei".`
@@ -10,7 +12,7 @@ async function sendSMS (payload) {
   const { telefonnummer } = payload
   const melding = genererMelding(payload)
   try {
-    const result = await handleSMS(telefonnummer, melding)
+    const result = await handleSMS(telefonnummer, melding, callerId)
     return { ...result, success: true }
   } catch (error) {
     return { success: false }
