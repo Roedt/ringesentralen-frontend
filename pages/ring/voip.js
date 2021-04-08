4634219a624a15e1b1f26b711e20b045d50e668a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-08 19:53:40 +0200

    Fixes #149 varsel på VoIP om brukeren ikke har telefonnummer (patch)

--- pages/ring/voip.js
+++ pages/ring/voip.js
@@ -164,6 +164,15 @@ function VoIP ({ telefonnummer }) {
     )
   }
 
+  if (!telefonnummer) {
+    const message = 'Brukeren har ikke telefonnummer. Da kan ikke vedkommende ringes.'
+    return (
+      <div className='mb-4 pb-4 border-b border-gray-200'>
+        <Warning message={message} />
+      </div>
+    )
+  }
+
   return (
     <div className='mb-4 pb-4 border-b border-gray-200'>
       <button onClick={() => handleVoipSetup()} className={`${voipReady ? 'hidden' : 'visible'} w-48 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
