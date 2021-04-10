7ff622c452c7ecda03e033d586e69e8f3917aaf5
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-10 15:12:07 +0200

    Fixes #151 oppdatert svarer. Legger inn fallback om vi ikke forstår svaret (patch)

--- pages/api/twilio/collect.js
+++ pages/api/twilio/collect.js
@@ -8,18 +8,28 @@ const VoiceResponse = twilio.twiml.VoiceResponse
 async function twilioCollectCall (request, response) {
   const payload = await request.body
   const { Digits, From } = payload
-  let melding = ''
+  let melding = 'Vi mottok ikke noe svar vi forstod, men det er helt greit. Godt valg! '
+  let success = false
   if (Digits === '1') {
     melding = svarerAlternativer.ja
+    success = true
   }
   if (Digits === '2') {
     melding = svarerAlternativer.nei
+    success = true
   }
-  await registrerSvar({ telefonnummer: From, digits: Digits })
   const twiml = new VoiceResponse()
-  twiml.play({
-    loop: 1
-  }, melding)
+  if (success) {
+    await registrerSvar({ telefonnummer: From, digits: Digits })
+    twiml.play({
+      loop: 1
+    }, melding)
+  } else {
+    twiml.say({
+      voice: 'Polly.Liv',
+      language: 'nb-NO'
+    }, melding)
+  }
   response.setHeader('Content-Type', 'application/xml')
   response.status(200).send(twiml.toString())
 }
