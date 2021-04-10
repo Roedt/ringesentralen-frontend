a2e5c57354e6bdd91a3159b84b6a5f9f7064f6d4
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-10 10:39:56 +0200

    Legger inn lenker til telefonsvarer-alternativene #151 (patch)

---
+++ pages/api/twilio/telefonsvarer-alternativer.js
@@ -0,0 +1,7 @@
+const svarAlternativer = {
+  svar: 'https://storage.googleapis.com/telefonsvarer/Telefonsvarer_1.mp3',
+  ja: 'https://storage.googleapis.com/telefonsvarer/Telefonsvarer_2.mp3',
+  nei: 'https://storage.googleapis.com/telefonsvarer/Telefonsvarer_3.mp3'
+}
+
+export default svarAlternativer
