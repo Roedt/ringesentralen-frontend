ea5e8f599084981e83aad32fe113b9394e8cb712
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-14 18:12:37 +0200

    Oppdaterer minipoll så den passer bedre for flere alternativer (patch)

--- components/minipoll.js
+++ components/minipoll.js
@@ -18,7 +18,7 @@ function Minipoll ({ tekst, tema, alternativer }) {
 
   function Button ({ alternativ, valgt }) {
     return (
-      <button type='button' onClick={() => registrerSvar(alternativ)} className={`w-24 mr-2 mt-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 ${valgt === alternativ ? 'bg-gray-100' : 'bg-white'} hover:bg-gray-50 focus:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+      <button type='button' onClick={() => registrerSvar(alternativ)} className={`w-36 mr-2 mt-2 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 ${valgt === alternativ ? 'bg-gray-100' : 'bg-white'} hover:bg-gray-50 focus:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
         <span>
           {alternativ}
         </span>
@@ -28,8 +28,7 @@ function Minipoll ({ tekst, tema, alternativer }) {
 
   return (
     <div className='my-4 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg'>
-      <h1 className='text-lg font-semibold mb-1'>Minipoll</h1>
-      <h2 className='text-lg mb-2'>{tekst}</h2>
+      <h1 className='text-lg mb-2'>{tekst}</h1>
       <div>
         {alternativer.map((alternativ, index) => <Button alternativ={alternativ} valgt={valgt} key={`poll-alternativ-${index}`} />)}
       </div>
