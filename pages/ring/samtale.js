0afb30d9df1fc152ec1a372df054eaf81f2cea27
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-14 17:13:28 +0200

    Sender med navnTilDenSomringes til ringemanus #160 (patch)

--- pages/ring/samtale.js
+++ pages/ring/samtale.js
@@ -17,17 +17,25 @@ function Samtale ({ accepted, data, user, setPerson }) {
   if (!kanViseKomponent) return null
 
   const { person } = data
-  const { telefonnummer, id, fylke, lokallagNavn } = person
+  const { telefonnummer, id, fylke, lokallagNavn, fornavn, etternavn } = person
+  const navnTilDenSomringes = `${fornavn} ${etternavn}`
 
   return (
     <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
+      {JSON.stringify(person, null, 2)}
       {telefonnummer && <VoIP telefonnummer={telefonnummer} />}
       <div className='mt-4 flex flex-col lg:flex-row'>
         <div className='flex-1 lg:pr-4 md:mb-4 sm:mb-4'>
           <ResultatSkjema id={id} setPerson={setPerson} modus={modus} telefonnummer={telefonnummer} />
         </div>
         <div className='flex-1 lg:pl-4 md:mb-4 sm:mb-4'>
-          <Ringemanus manus={fylke} modus={modus} lokalLag={lokallagNavn} navn={`${user?.navn}`} />
+          <Ringemanus
+            manus={fylke}
+            modus={modus}
+            lokalLag={lokallagNavn}
+            navn={`${user?.navn}`}
+            navnTilDenSomringes={navnTilDenSomringes}
+          />
         </div>
       </div>
     </div>
