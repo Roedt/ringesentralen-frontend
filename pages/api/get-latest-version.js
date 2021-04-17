a99316c6b0b906e5616a3f54506da2cd0a9635c0
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-17 07:46:46 +0200

    Fixes #179 cache mot ny versjon ute (patch)

--- pages/api/get-latest-version.js
+++ pages/api/get-latest-version.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import NodeCache from 'node-cache'
+const cache = new NodeCache({ stdTTL: 3600 })
 
 const latestUrl = 'https://api.github.com/repos/roedt/ringesentralen-frontend/releases/latest'
 const config = {
@@ -8,18 +10,28 @@ const config = {
 }
 
 async function isLatesVersion (request, response) {
-  delete axios.defaults.headers.common.Authorization
-  try {
-    const { data } = await axios.get(latestUrl, config)
-    const { tag_name: latestVersion } = data
+  const latest = cache.get('latest')
+  if (latest === undefined) {
+    console.log('cache miss')
+    delete axios.defaults.headers.common.Authorization
+    try {
+      const { data } = await axios.get(latestUrl, config)
+      const { tag_name: latestVersion } = data
+      cache.set('latest', latestVersion)
+      response.json({
+        latestVersion,
+        success: true
+      })
+    } catch (error) {
+      console.error(error)
+      response.json({
+        success: false
+      })
+    }
+  } else {
     response.json({
-      latestVersion,
+      latestVersion: latest,
       success: true
-    })
-  } catch (error) {
-    console.error(error)
-    response.json({
-      success: false
     })
   }
 }
