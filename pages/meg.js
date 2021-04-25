f4fc4ed5c1f1fd7892e1510ca1caa1aa23aab748
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-25 08:02:51 +0200

    Spesisfiserer hvilke verdier som er fra sesjon #194 (patch)

--- pages/meg.js
+++ pages/meg.js
@@ -74,11 +74,11 @@ function MinProfil ({ profil, session, mineLokallag }) {
               </dd>
             </div>
             <div className='py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-              <dt className='text-sm font-medium text-gray-500'>Aktivt lokallag</dt>
+              <dt className='text-sm font-medium text-gray-500'>Aktivt lokallag i sesjon</dt>
               <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>{hentAktivtLokallagNavn(aktivtLokallag)}</dd>
             </div>
             <div className='py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-              <dt className='text-sm font-medium text-gray-500'>Aktivt ringemodus</dt>
+              <dt className='text-sm font-medium text-gray-500'>Aktivt ringemodus i sesjon</dt>
               <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>{aktivtModus}</dd>
             </div>
             <div className='py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
