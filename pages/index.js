2d3d333262c62b590f2e8a1956813f4a6738b2f2
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-04-27 19:23:49 +0200

    Fixes #201 Laste ned dashboard som csv (patch)

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { saveAs } from 'file-saver'
+import { Parser } from 'json2csv'
 
 import useUser from '../lib/useUser'
 import Layout from '../components/layout'
@@ -57,6 +59,20 @@ function Linje ({ lokallag, igjenAaRinge, personerSomKanRinges, totaltInklRingte
 }
 
 function Dashboard ({ dashboard }) {
+  function lastNedCSV () {
+    const json2csvParser = new Parser()
+    const { statusliste } = dashboard
+    const data = statusliste.map(linje => Object.assign({}, {
+      lokallag: linje.lokallag.navn,
+      igjenAaRinge: linje.igjenAaRinge,
+      personerSomKanRinges: linje.personerSomKanRinges,
+      totaltInklRingte: linje.totaltInklRingte
+    }))
+    const csv = json2csvParser.parse(data)
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' })
+    saveAs(blob, 'dashboard.csv')
+  }
+
   if (!dashboard) return null
   const { statusliste } = dashboard
   /*
@@ -70,6 +86,16 @@ function Dashboard ({ dashboard }) {
   */
   return (
     <>
+      <div className='flex justify-end'>
+        <button type='button' onClick={() => lastNedCSV()} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10' />
+          </svg>
+          <span>
+            Last ned
+          </span>
+        </button>
+      </div>
       {statusliste.map(data => <Linje {...data} key={data.lokallag.id} />)}
     </>
   )
