88e66836c1cf11b601e381f3cdaf3cdc95f69b1d
Mads Opheim <mads@fostad.net>
2021-05-05 17:38:11 +0200

    Tar høgde for at frivillig kan vera udefinert (patch)

--- pages/frivilligbasen/frivillig.js
+++ pages/frivilligbasen/frivillig.js
@@ -2,6 +2,7 @@ import Aktiviteter from './aktiviteter'
 
 function Frivillig ({ data }) {
   const { frivillig, person, aktiviteter } = data
+  if (!frivillig) return (<div />)
   return (
     <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
       <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
