b5600671fb556ec62505741e734a78e067d8bf70
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-06 15:58:08 +0200

    Legger til kontaktskjema for frivillige #214 (patch)

--- pages/frivilligbasen/frivillig.js
+++ pages/frivilligbasen/frivillig.js
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 
 import Aktiviteter from './aktiviteter'
+import KontaktSkjema from './kontaktSkjema'
 
 function Frivillig ({ data }) {
+  const [visSkjema, setVisSkjema] = useState()
+
   if (!data) return null
   const { frivillig, person, aktiviteter, fylke, lokallag } = data
   if (!frivillig) return null
+
   return (
     <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
       <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
@@ -12,7 +17,7 @@ function Frivillig ({ data }) {
           <h3 className='text-lg leading-6 font-medium text-gray-900'>{person.fornavn} {person.etternavn}</h3>
         </div>
         <div className='ml-4 mt-2 flex-shrink-0'>
-          <button type='button' className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <button type='button' onClick={() => setVisSkjema(!visSkjema)} className={`${!visSkjema ? 'visible' : 'invisible'} w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
             <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' />
             </svg>
@@ -22,6 +27,7 @@ function Frivillig ({ data }) {
           </button>
         </div>
       </div>
+      <KontaktSkjema frivillig={frivillig} visSkjema={visSkjema} setVisSkjema={setVisSkjema} />
       <div className='flex items-center'>
         <div className='ml-4'>
           <div className='text-sm text-gray-900'>
---
+++ pages/frivilligbasen/kontaktSkjema.js
@@ -0,0 +1,41 @@
+function KontaktSkjema ({ visSkjema, frivillig, setVisSkjema }) {
+  if (!visSkjema) return null
+
+  return (
+    <div className='sm:col-span-6'>
+      <label htmlFor='about' className='block text-l font-medium text-gray-700'>
+        Kontaktlogg
+      </label>
+      <div className='mt-1'>
+        <textarea
+          id='about'
+          name='about'
+          rows={5}
+          className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'
+          defaultValue=''
+        />
+      </div>
+      <p className='mt-2 text-l text-gray-500'>Skriv inn punkter etter samtalen</p>
+      <div className='flex justify-end'>
+        <button type='button' onClick={() => setVisSkjema(!visSkjema)} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' />
+          </svg>
+          <span>
+            Lagre
+          </span>
+        </button>
+        <button type='button' onClick={() => setVisSkjema(!visSkjema)} className='w-48 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-xl font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' />
+          </svg>
+          <span>
+            Avbryt
+          </span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default KontaktSkjema
