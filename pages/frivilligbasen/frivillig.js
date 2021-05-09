839234f4a0be7af25a875a8b337d8fe88a7db5ad
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-09 08:24:44 +0200

    Fikser breakpoint #214 (patch)

--- pages/frivilligbasen/frivillig.js
+++ pages/frivilligbasen/frivillig.js
@@ -41,7 +41,7 @@ function Frivillig ({ data }) {
           </div>
         </div>
         <div className='ml-4 mt-2 flex-shrink-0'>
-          <button type='button' onClick={() => setVisSkjema(!visSkjema)} className={`${!visSkjema ? 'visible' : 'invisible'} w-36 ml-3 mb-4 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-l font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+          <button type='button' onClick={() => setVisSkjema(!visSkjema)} className={`${!visSkjema ? 'visible' : 'invisible'} w-36 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-l font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
             <svg className='-ml-1 mr-2 h-5 w-5 text-gray-400' xmlns='https://www.w3.org/2000/svg' fill='none' stroke='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' />
             </svg>
@@ -51,7 +51,7 @@ function Frivillig ({ data }) {
           </button>
         </div>
       </div>
-      <div className='-mt-2 text-gray-700 text-sm'>
+      <div className='md:-mt-2 text-gray-700 text-sm'>
         <p>{genererTagLine(frivillig)}</p>
         <p>{frivillig.fortellLittOmDegSelv}</p>
       </div>
