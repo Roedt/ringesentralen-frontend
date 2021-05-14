6969e8f4e1863af692cea2957d7b1892da633377
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-14 07:56:31 +0200

    Legger til egen toast-context #197 (patch)

---
+++ contexts/toast-context.js
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+import Toast from '../components/ui/toast'
+
+const ToastContext = createContext()
+
+function ToastProvider ({ children }) {
+  const [toastMelding, setToastMelding] = useState()
+
+  useEffect(() => {
+    if (toastMelding) {
+      setTimeout(() => {
+        setToastMelding(false)
+      }, 2000)
+    }
+  }, [toastMelding])
+
+  return (
+    <ToastContext.Provider value={{ setToastMelding }}>
+      {children}
+      <div
+        aria-live='assertive'
+        className='fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start'
+      >
+        <div className='w-full flex flex-col items-center space-y-4 sm:items-end'>
+          <Toast melding={toastMelding} />
+        </div>
+      </div>
+    </ToastContext.Provider>
+  )
+}
+
+function useToast () {
+  const context = useContext(ToastContext)
+  if (context === undefined) {
+    throw new Error('useToast må brukes under en ToastProvider')
+  }
+  return context
+}
+
+export {
+  ToastProvider,
+  useToast
+}
