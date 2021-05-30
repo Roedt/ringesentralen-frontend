2a1a423bc50e59d5396d15e2ac43d9e1a2a51b9d
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-30 08:02:53 +0200

    Tester alle alert-komponentene #98 (patch)

--- components/ui/alerts.test.js
+++ components/ui/alerts.test.js
@@ -3,12 +3,33 @@
  */
 
 import { render, screen } from '@testing-library/react'
-import { Danger } from './alerts'
+import { Danger, Info, Success, Warning } from './alerts'
 
 describe('Tester de ulike alert komponentene', () => {
   test('Danger viser teksten som sendes inn', () => {
     const melding = 'Dette er Danger'
     const { container } = render(<Danger message={melding} />)
+    expect(container).not.toBeEmptyDOMElement()
+    expect(screen.getByText(melding)).toBeInTheDocument()
+  })
+
+  test('Info viser teksten som sendes inn', () => {
+    const melding = 'Dette er Info'
+    const { container } = render(<Info message={melding} />)
+    expect(container).not.toBeEmptyDOMElement()
+    expect(screen.getByText(melding)).toBeInTheDocument()
+  })
+
+  test('Success viser teksten som sendes inn', () => {
+    const melding = 'Dette er Success'
+    const { container } = render(<Success message={melding} />)
+    expect(container).not.toBeEmptyDOMElement()
+    expect(screen.getByText(melding)).toBeInTheDocument()
+  })
+
+  test('Warning viser teksten som sendes inn', () => {
+    const melding = 'Dette er Warning'
+    const { container } = render(<Warning message={melding} />)
     expect(container).not.toBeEmptyDOMElement()
     expect(screen.getByText(melding)).toBeInTheDocument()
   })
