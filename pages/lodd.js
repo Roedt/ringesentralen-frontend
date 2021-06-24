d1b5beab666e1e2e35bb38232628b31e4aabcb4e
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-06-24 15:50:04 +0200

    Legger til side for lodd #271

---
+++ pages/lodd.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+function Lodd () {
+  return (
+    <Layout pageTitle='Lodd'>
+      <Head>
+        <title>Loddtrekning</title>
+      </Head>
+      <div>
+        Loddorama
+      </div>
+    </Layout>
+  )
+}
+
+export default Lodd
