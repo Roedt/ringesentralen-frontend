f7e924794832dd156fed13eea9b12967559f03f6
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-09-18 08:20:09 +0200

    Setter medlemmer til default aktivtModus med innlogging #349 (patch)

--- pages/api/login.js
+++ pages/api/login.js
@@ -29,7 +29,7 @@ async function login (request, response) {
       lokallag,
       token,
       expires: new Date().getTime() + 36000000,
-      aktivtModus: 'velgere',
+      aktivtModus: 'medlemmer',
       aktivtLokallag: lokallag,
       isAuthenticated: true
     }
