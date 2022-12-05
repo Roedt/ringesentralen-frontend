9c4886fb8f8810481c768ab8b9554910ceab5e38
Mads Opheim <mads.opheim@gmail.com>
2022-12-05 23:32:43 +0100

    Imports

--- pages/forum/editor.js
+++ pages/forum/editor.js
@@ -1,8 +1,8 @@
-import ExampleTheme from './themes/ExampleTheme'
+import ExampleTheme from '../../components/forum/themes/ExampleTheme'
 import { LexicalComposer } from '@lexical/react/LexicalComposer'
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
-import ToolbarPlugin from '../../components/forum/plugins/plugins/ToolbarPlugin'
+import ToolbarPlugin from '../../components/forum/plugins/ToolbarPlugin'
 import { HeadingNode, QuoteNode } from '@lexical/rich-text'
 import { TableCellNode, TableNode, TableRowNode } from '@lexical/table'
 import { ListItemNode, ListNode } from '@lexical/list'
@@ -10,7 +10,7 @@ import { AutoLinkNode, LinkNode } from '@lexical/link'
 import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin'
 import { ListPlugin } from '@lexical/react/LexicalListPlugin'
 import ListMaxIndentLevelPlugin from '../../components/forum/plugins/ListMaxIndentLevelPlugin'
-import AutoLinkPlugin from '../../components/forum/plugins/plugins/AutoLinkPlugin'
+import PlaygroundAutoLinkPlugin from '../../components/forum/plugins/AutoLinkPlugin'
 import Tekstboksen from './tekstboksen'
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
 
@@ -47,7 +47,7 @@ const Editor = ({ listener, readOnly, eksisterendeInnhold }) => {
           {!readOnly && <OnChangePlugin onChange={listener} />}
           <ListPlugin />
           <LinkPlugin />
-          <AutoLinkPlugin />
+          <PlaygroundAutoLinkPlugin />
           <ListMaxIndentLevelPlugin maxDepth={7} />
         </div>
       </div>
