0bf3a5b8f836032220c472e244257d4d273de129
Mads Opheim <mads.opheim@gmail.com>
2022-12-21 22:50:40 +0100

    Viss eingongskode-feltet visast, er det obligatorisk

--- pages/login.js
+++ pages/login.js
@@ -139,6 +139,7 @@ function Login () {
                 <input
                 id='engangskode' type='text' name='engangskode' placeholder='Engangskode'
                 className='block w-full py-3 px-1 mt-2 mb-4 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200'
+                required
                 />
                 </>
               }
