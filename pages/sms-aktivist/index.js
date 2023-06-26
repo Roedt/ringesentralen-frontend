f00a241fafa9f1cc23d54062a9c5634f4b772f20
Mads Opheim <mads@fostad.net>
2023-06-26 20:21:50 +0200

    Namneendringar og opprydding

--- pages/sms-aktivist/index.js
+++ pages/sms-aktivist/index.js
@@ -3,14 +3,15 @@ import { useState } from 'react'
 import Head from 'next/head'
 
 import SMSKvittering from './kvittering'
+import SMSSkjema from './skjema'
 
-function SMSFrivillig () {
+function SMSAktivist () {
   const [success, setSuccess] = useState()
 
   return (
     <>
       <Head>
-        <title>Meld deg som SMS-frivillig for Rødt</title>
+        <title>Meld deg som SMS-aktivist for Rødt</title>
       </Head>
       <div className='relative bg-white'>
         {success && <Confetti />}
@@ -21,16 +22,16 @@ function SMSFrivillig () {
           <header className='bg-roedt py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12'>
             <div className='max-w-lg mx-auto'>
               <h1 className='text-2xl font-extrabold tracking-tight text-white sm:text-3xl'>
-                Bli SMS-frivillig
+                Bli SMS-aktivist
               </h1>
               <p className='mt-3 text-lg leading-6 text-white'>
-                Har du lyst til å bli SMS-frivillig for Rødt?
+                Har du lyst til å bli SMS-aktivist for Rødt?
               </p>
               <p className='mt-3 text-lg leading-6 text-white'>
                 Fyll ut skjemaet, så blir du det!
               </p>
               <p className='mt-6 text-lg text-white'>
-                Vil du bidra til tidenes valgkamp for Rødt? <a href='https://www.rødt.no/bidra' className='text-white underline'>Les mer på rødt.no/bidra</a>.
+                Vil du bidra til tidenes valgkamp for Rødt?<br /><a href='https://www.rødt.no/bidra' className='text-white underline'>Les mer på rødt.no/bidra</a>.
               </p>
             </div>
           </header>
@@ -47,4 +48,4 @@ function SMSFrivillig () {
   )
 }
 
-export default SMSFrivillig
+export default SMSAktivist
---
+++ pages/sms-aktivist/skjema.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+import generatePayload from '../../lib/generate-payload'
+import useRecaptcha from '../../lib/useRecaptcha'
+
+import Button from '../../components/ui/button'
+
+function SMSSkjema ({ setSuccess }) {
+  const [loading, setLoading] = useState()
+  const { getToken } = useRecaptcha()
+
+  async function handleSubmit (event) {
+    event.preventDefault()
+    setLoading(true)
+    const token = await getToken({ action: 'sms-aktivist' })
+    const form = document.getElementById('sms-aktivist-skjema')
+    const payload = generatePayload(form)
+    payload.token = token
+    await axios.post('/api/sms-aktivist', payload)
+    setLoading(false)
+    form.reset()
+    setSuccess(true)
+  }
+
+  return (
+    <form id='sms-aktivist-skjema' onSubmit={handleSubmit} className='grid grid-cols-1 gap-y-6'>
+      <div>
+        <label htmlFor='fornavn' className='sr-only'>Fornavn {''}</label>
+        <input type='text' name='fornavn' id='fornavn' autoComplete='given-name' required className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md' placeholder={`Fornavn`} />
+      </div>
+      <div>
+        <label htmlFor='etternavn' className='sr-only'>Etternavn {''}</label>
+        <input type='text' name='etternavn' id='etternavn' autoComplete='family-name' required className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md' placeholder={`Etternavn`} />
+      </div>
+      <div>
+        <label htmlFor='postnummer' className='sr-only'>Postnummer {''}</label>
+        <input type='text' name='postnummer' id='postnummer' autoComplete='postal-code' required className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md' placeholder={`Postnummer`} />
+      </div>
+      <div>
+        <label htmlFor='telefonnummer' className='sr-only'>Telefonnummer {''}</label>
+        <input type='text' name='telefonnummer' id='telefonnummer' autoComplete='tel' required className='block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md' placeholder={`Telefonnummer`} />
+      </div>
+      <div>
+        <Button
+          type='submit'
+          loading={loading}
+        >
+          Meld deg som SMS-aktivist!
+        </Button>
+      </div>
+    </form>
+  )
+}
+
+export default SMSSkjema
