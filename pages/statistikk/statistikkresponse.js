9db70df0e811495446fd25b78ec113def2662187
Mads Opheim <mads@fostad.net>
2023-08-10 22:21:35 +0200

    Viser statistikk for antal samtalar i denne valkampen

--- pages/statistikk/statistikkresponse.js
+++ pages/statistikk/statistikkresponse.js
@@ -70,11 +70,55 @@ const StatistikkResponse = ({ statistikk, user }) => {
     )
   }
 
+  function Linje ({ lokallag, samtalerStarta }) {
+    return (
+      <tr>
+        <td className='px-6 py-4 whitespace-nowrap'>
+          <div className='flex items-center'>
+            <div className='ml-4'>
+              <div className='text-sm font-medium text-gray-900'>
+                {lokallag.navn}
+              </div>
+            </div>
+          </div>
+        </td>
+        <td className='px-6 py-4 whitespace-nowrap'>
+          <div className='text-sm text-gray-900'>{samtalerStarta}</div>
+        </td>
+      </tr>
+    )
+  }
+
+  const RingteIValkampen2023 = () => {
+    if (!statistikk) return null
+    return (
+      <div>
+        <h2 className='text-xl font-semibold mt-12'>Ringte i valkampen 2023-statistikk</h2>
+        <table className='min-w-full divide-y divide-gray-200'>
+          <thead className='bg-gray-50'>
+          <tr>
+            <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+              Lokallag
+            </th>
+            <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+              Samtaler starta
+            </th>
+          </tr>
+          </thead>
+          <tbody className='bg-white divide-y divide-gray-200'>
+          {statistikk.ringteIValkampen2023.map(linje => <Linje {...linje} key={linje.lokallag.id} />)}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   return (
     <div>
       {erRinger && SamtalerStatistikk(statistikk)}
       {erAdmin && RingereStatistikk(statistikk)}
       {erAdmin && PersonerStatistikk(statistikk)}
+      {erAdmin && RingteIValkampen2023(statistikk)}
     </div>
   )
 }
