d319d09804fae82987ff86a33d92f1be20cb8591
Mads Opheim <mads@fostad.net>
2023-08-10 23:07:23 +0200

    Viser statistikk for antal samtalar i denne valkampen

--- pages/statistikk/statistikkresponse.js
+++ pages/statistikk/statistikkresponse.js
@@ -70,20 +70,20 @@ const StatistikkResponse = ({ statistikk, user }) => {
     )
   }
 
-  function Linje ({ lokallag, samtalerStarta }) {
+  function Linje ({ first, second }) {
     return (
       <tr>
         <td className='px-6 py-4 whitespace-nowrap'>
           <div className='flex items-center'>
             <div className='ml-4'>
               <div className='text-sm font-medium text-gray-900'>
-                {lokallag}
+                {first}
               </div>
             </div>
           </div>
         </td>
         <td className='px-6 py-4 whitespace-nowrap'>
-          <div className='text-sm text-gray-900'>{samtalerStarta}</div>
+          <div className='text-sm text-gray-900'>{second}</div>
         </td>
       </tr>
     )
