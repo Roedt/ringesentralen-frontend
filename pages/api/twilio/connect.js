27738ef5f9c541368f7e86350b146c0617907d35
Mads Opheim <mads@fostad.net>
2024-05-04 09:00:32 +0200

    Ryddar bort telefonsvarfunksjonaliteten som vi ikkje bruker lenger

--- pages/api/twilio/connect.js
+++ pages/api/twilio/connect.js
@@ -1,7 +1,6 @@
 import twilio from 'twilio'
 
 import fixTelefonNummer from '../../../lib/fix-telefonnummer'
-import svarerAlternativer from './telefonsvarer-alternativer'
 
 const VoiceResponse = twilio.twiml.VoiceResponse
 
@@ -15,24 +14,6 @@ async function twilioConnectCall (request, response) {
     // Telefoner fra Ringesentralen over VoIP
     const dial = twiml.dial({ callerId: process.env.TWILIO_CALLER_ID })
     dial.number({}, fixTelefonNummer(telefonnummer))
-  } else {
-    // Telefoner som ringer tilbake skal treffe telefonsvareren vår
-    // Først setter vi opp tastefunksjonen som tar imot input fra tastene
-    // Den timer ut etter 5 sekunder og er ferdig etter at ett tall er mottatt
-    const gather = twiml.gather({
-      input: 'dtmf',
-      timeout: 5,
-      numDigits: 1,
-      action: '/api/twilio/collect'
-    })
-    // Så setter vi opp stemmen som skal leses opp først
-    gather.play({
-      loop: 1
-    }, svarerAlternativer.svar)
-    // Dersom innsamling av data timer ut sendes data videre likevel
-    twiml.redirect('/api/twilio/collect')
-    // Dette er den gamle koden for å redirecte til et annet telefonnummer
-    // twiml.dial(process.env.TWILIO_INCOMING_HANDLER)
   }
   response.setHeader('Content-Type', 'application/xml')
   response.status(200).send(twiml.toString())
--- pages/api/twilio/telefonsvarer-alternativer.js
+++
@@ -1,7 +0,0 @@
-const svarAlternativer = {
-  svar: 'https://storage.googleapis.com/telefonsvarer/Telefonsvarer_1.mp3',
-  ja: 'https://storage.googleapis.com/telefonsvarer/Telefonsvarer_2.mp3',
-  nei: 'https://storage.googleapis.com/telefonsvarer/Telefonsvarer_3.mp3'
-}
-
-export default svarAlternativer
