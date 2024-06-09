e771aedc68eda40ebca4541ef89f05365cb0fee4
Mads Opheim <mads@fostad.net>
2024-06-09 19:03:21 +0200

    Skrivefeil

--- pages/meg.js
+++ pages/meg.js
@@ -32,7 +32,7 @@ function MinProfil ({ profil, session, mineLokallag }) {
       {rollerProfil.toString() !== rollerSesjon.toString() && <Warning message={message} />}
       <div className='bg-white shadow overflow-hidden sm:rounded-lg'>
         <div className='px-4 py-5 sm:px-6'>
-          <h3 className='text-lg leading-6 font-medium text-gray-900'>Meg i Ringesentalen</h3>
+          <h3 className='text-lg leading-6 font-medium text-gray-900'>Meg i Ringesentralen</h3>
           <p className='mt-1 max-w-2xl text-sm text-gray-500'>Informasjon fra profil og innlogget sesjon</p>
         </div>
         <div className='border-t border-gray-200 px-4 py-5 sm:p-0'>
